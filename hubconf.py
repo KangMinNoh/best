d02043687a75886f078f897b5e03be5a6f82d8b6
KangMinNoh <137526675+KangMinNoh@users.noreply.github.com>
2023-06-24 01:55:40 +0900

    Add files via upload

--- hubconf.py
+++ hubconf.py
@@ -1,19 +1,19 @@
 import torch
-import torchvision.models as models
+import requests
 
-class YOLOv7Tiny(torch.nn.Module):
-    def __init__(self):
-        super(YOLOv7Tiny, self).__init__()
-        self.model = models.yolov5s()
-
-    def forward(self, x):
-        return self.model(x)
-
+# 가중치 파일을 다운로드하여 로드하는 함수
 def load_weights(model, model_weights_url):
-    # 가중치 파일을 다운로드하고 모델에 로드하는 함수 구현
+    response = requests.get(model_weights_url)
+    response.raise_for_status()
+    with open('best.pt', 'wb') as f:
+        f.write(response.content)
+    state_dict = torch.load('best.pt', map_location=torch.device('cpu'))
+    model.load_state_dict(state_dict)
+    return model
 
+# 호출 가능한 함수로 가중치를 로드하는 예시 함수
 def yolov7tiny(pretrained=False, **kwargs):
-    model = YOLOv7Tiny()
+    model = YourCustomModel()  # 실제 모델 클래스나 함수로 대체해야 합니다.
     if pretrained:
         model_weights_url = 'https://github.com/KangMinNoh/best/raw/main/best.pt'
         model = load_weights(model, model_weights_url)
