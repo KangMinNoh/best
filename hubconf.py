386bc6a9b61b692a86af7750a4e5d7790a62f040
KangMinNoh <137526675+KangMinNoh@users.noreply.github.com>
2023-06-24 02:48:51 +0900

    Add files via upload

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,16 @@
 import torch
 import requests
 
+# YOLOv7-tiny 모델 클래스
+class YOLOv7Tiny(torch.nn.Module):
+    def __init__(self):
+        super(YOLOv7Tiny, self).__init__()
+        # 모델 아키텍처 정의
+
+    def forward(self, x):
+        # 순전파 정의
+        pass
+
 # 가중치 파일을 다운로드하여 로드하는 함수
 def load_weights(model, model_weights_url):
     response = requests.get(model_weights_url)
@@ -13,9 +23,9 @@ def load_weights(model, model_weights_url):
 
 # 호출 가능한 함수로 가중치를 로드하는 예시 함수
 def yolov7tiny(pretrained=False, **kwargs):
-    model = YOLOv7Tiny()  # 실제 모델 클래스나 함수로 대체해야 합니다.
+    model = YOLOv7Tiny()  # YOLOv7-tiny 모델 객체 생성
     if pretrained:
-        model_weights_url = 'https://github.com/KangMinNoh/best/best.pt'
+        model_weights_url = 'https://github.com/KangMinNoh/best/raw/main/best.pt'
         model = load_weights(model, model_weights_url)
     return model
 
