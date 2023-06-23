1592380e34392b96ef5ad4206e5bcdbc24397243
KangMinNoh <137526675+KangMinNoh@users.noreply.github.com>
2023-06-24 02:13:44 +0900

    Add files via upload

--- hubconf.py
+++ hubconf.py
@@ -7,15 +7,15 @@ def load_weights(model, model_weights_url):
     response.raise_for_status()
     with open('best.pt', 'wb') as f:
         f.write(response.content)
-    state_dict = torch.load('yolov7-tiny.pt', map_location=torch.device('cpu'))
+    state_dict = torch.load('best.pt', map_location=torch.device('cpu'))
     model.load_state_dict(state_dict)
     return model
 
 # 호출 가능한 함수로 가중치를 로드하는 예시 함수
 def yolov7tiny(pretrained=False, **kwargs):
-    model = yolov7-tiny.pt()  # 실제 모델 클래스나 함수로 대체해야 합니다.
+    model = YOLOv7Tiny()  # 실제 모델 클래스나 함수로 대체해야 합니다.
     if pretrained:
-        model_weights_url = 'https://github.com/KangMinNoh/best.git'
+        model_weights_url = 'https://github.com/KangMinNoh/best/best.pt'
         model = load_weights(model, model_weights_url)
     return model
 
